08ee4ae1a5a65a3c0b766e68c7f3207b359e5004
dondavan <yeezushugh@gmail.com>
2024-01-21 15:54:28 +0100

    Add input

--- ass2/Parallel_MPI/load_module.sh
+++ ass2/Parallel_MPI/load_module.sh
@@ -2,6 +2,6 @@
 #SBATCH --partition=thin_course
 #SBATCH -t 3:00 --ntasks=16
 
-module purge
-module load 2023
-module load MPICH/4.1.2-GCC-12.3.0
+module purge \
+module load 2023 \
+module load MPICH/4.1.2-GCC-12.3.0 \
