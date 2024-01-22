25f3aeb4cf2b7b919eb68ca5c47cba84440471f4
dondavan <yeezushugh@gmail.com>
2024-01-22 11:20:53 +0100

    MPI

--- ass2/Parallel_MPI/gameoflife.c
+++ ass2/Parallel_MPI/gameoflife.c
@@ -169,13 +169,14 @@ void simulate(const struct parameters *p,struct results *r)
     {
         if(MPI_rank!=i)sleep(1);
         /* Output Board for Report*/
-        else
-        for(i_row = row_start; i_row <= row_end; i_row++){
+        else{
+            for(i_row = row_start; i_row <= row_end; i_row++){
                 for(j_col = 0; j_col < col; j_col++){
-                printf("%hhu",old[i_row*col + j_col]);  
-                }
+                    printf("%hhu",old[i_row*col + j_col]);  
+                    }
                 printf("\n");
             }
+        }
     }
     printf("That took %f seconds\n",endtime-starttime);
     /* Output Board for Report
