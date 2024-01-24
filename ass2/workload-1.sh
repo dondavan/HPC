dd7eeeae813291f237742b1e731044b437baa501
dondavan <yeezushugh@gmail.com>
2024-01-24 14:20:18 +0100

    test workload

---
+++ ass2/workload-1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name="pi"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=00:20:00
+#SBATCH --partition=thin_course
+#SBATCH --output=pi_%j.out
+#SBATCH --error=pi_%j.err
+
+module purge
+module load 2022
+module load GCCcore-11.3.0
+
+echo "OpenMP parallelism"
+echo
+
+for i in `seq 1 20`
+do
+    echo "======== Run : " $i
+    export OMP_NUM_THREADS=1
+
+    echo "CPUS: " $OMP_NUM_THREADS
+    echo "CPUS: " $OMP_NUM_THREADS >&2
+
+    ./pi -i31250000
+    ./pi -i62500000
+    ./pi -i125000000
+    ./pi -i250000000
+    ./pi -i500000000
+    ./pi -i1000000000
+    ./pi -i2000000000
+    
+    echo "DONE "
+done
