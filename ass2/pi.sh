987fa67ad8e7bdedc9bc0d6f50368f7e9dec0e67
dondavan <yeezushugh@gmail.com>
2024-01-24 10:46:23 +0100

    new data

--- ass2/pi.sh
+++ ass2/pi.sh
@@ -4,11 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --time=00:10:00
-#SBATCH --mem=100M
-#SBATCH --partition=gpu_course
+#SBATCH --partition=thin_course
 #SBATCH --output=pi_%j.out
 #SBATCH --error=pi_%j.err
-
 
 module purge
 module load 2022
@@ -17,9 +15,13 @@ module load GCCcore-11.3.0
 echo "OpenMP parallelism"
 echo
 
-  export OMP_NUM_THREADS=1
+for ncores in `seq 8 4 48`
+
+do
+  export OMP_NUM_THREADS=$ncores
 
   echo "CPUS: " $OMP_NUM_THREADS
   echo "CPUS: " $OMP_NUM_THREADS >&2
   ./pi
   echo "DONE "
+done
