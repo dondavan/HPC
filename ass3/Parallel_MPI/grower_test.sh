7fe8472c9286940c4caceef54314efb3c49c5718
Hang Xu <scur0330@int5.local.snellius.surf.nl>
2024-01-27 16:23:29 +0100

    with result

--- ass3/Parallel_MPI/grower_test.sh
+++ ass3/Parallel_MPI/grower_test.sh
@@ -6,4 +6,4 @@ module purge
 module load 2023
 module load MPICH/4.1.2-GCC-12.3.0
 
-mpirun ./Parallel_MPI -n400 -m400 -x200 -y200 -i9 -finput/grower.txt
+mpirun ./Parallel_MPI -n400 -m400 -x200 -y200 -i10 -finput/grower.txt
