8faa1a120e93cc2e109adb94e50e85367d054685
dondavan <yeezushugh@gmail.com>
2024-01-28 11:39:01 +0100

    exp

--- ass3/Parallel_MPI/grower_4.sh
+++ ass3/Parallel_MPI/grower_4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #SBATCH --partition=thin_course
-#SBATCH -t 20:00 --ntasks=1
+#SBATCH -t 20:00 --ntasks=4
 
 module purge
 module load 2023
@@ -9,5 +9,5 @@ module load MPICH/4.1.2-GCC-12.3.0
 for i in `seq 1 10`
 do
     echo "======== Run : " $i
-    mpirun ./Parallel_MPI -n3000 -m3000 -x1500 -y1500 -i100 -finput/grower.txt
+    mpirun ./Parallel_MPI -n3000 -m3000 -x1500 -y1500 -i5000 -finput/grower.txt
 done
