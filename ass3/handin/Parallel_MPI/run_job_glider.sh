d3ba4ffe998a56e6cd41ebf9477423d738b2f8f2
dondavan <yeezushugh@gmail.com>
2024-01-29 23:14:18 +0100

    handin

--- ass3/handin/Parallel_MPI/run_job_glider.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-#SBATCH --partition=thin_course
-#SBATCH -t 3:00 --ntasks=4
-
-module purge
-module load 2023
-module load MPICH/4.1.2-GCC-12.3.0
-
-mpirun ./Parallel_MPI -n10 -m10 -x3 -y3 -i2 -finput/glider.txt
